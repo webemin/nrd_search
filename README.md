d65572941db2f798041c35a0e042dcb0c33e9de6
webemin <55952967+webemin@users.noreply.github.com>
2022-05-31 17:37:43 +0300

    platform selection

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # nrd_search
- search in new registered domains
+search in new registered domains downloaded from usom and whoisds domains using selenium.
+------
+# Usage
+- cd nrd_search
+- pip install -r requirements.txt
+- Edit conf.txt file up to yourself
+- Append keywords to keywords.txt file (add one empty line at the end of the file)
+- ptyhon nrd.py
+------
+note: Don't forget to use the same version of google chrome and chromedriver.exe
--- nrd.py
+++ nrd.py
@@ -35,58 +35,115 @@ conf_default_download_dir = conf_str.split("\n")[0].split("=")[1]
 print(conf_default_download_dir)
 
 #kullanıcıdan tarih girişi alınsın mı sorgusu
-if input("Tarih varsayılan (dün ve bugün) olarak kalsın mı?(y/n)") == "n":
+welcome = (""" 
 
-    date1_i = input("Başlangıç Tarihini Giriniz (YIL-AY-GÜN): ")
-    date1_data = date1_i.split("-")
+███╗░░██╗██████╗░██████╗░░░░░░░░██████╗███████╗░█████╗░██████╗░░█████╗░██╗░░██╗
+████╗░██║██╔══██╗██╔══██╗░░░░░░██╔════╝██╔════╝██╔══██╗██╔══██╗██╔══██╗██║░░██║
+██╔██╗██║██████╔╝██║░░██║█████╗╚█████╗░█████╗░░███████║██████╔╝██║░░╚═╝███████║
+██║╚████║██╔══██╗██║░░██║╚════╝░╚═══██╗██╔══╝░░██╔══██║██╔══██╗██║░░██╗██╔══██║
+██║░╚███║██║░░██║██████╔╝░░░░░░██████╔╝███████╗██║░░██║██║░░██║╚█████╔╝██║░░██║
+╚═╝░░╚══╝╚═╝░░╚═╝╚═════╝░░░░░░░╚═════╝░╚══════╝╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝
 
-    date2_i = input("Bitiş Tarihini Giriniz (YIL-AY-GÜN): ")
-    dates2_data = date2_i.split("-")
 
-    date1_using = datetime(int(date1_data[0]), int(date1_data[1]), int(date1_data[2]))
-    date2_using = datetime(int(dates2_data[0]), int(dates2_data[1]), int(dates2_data[2]))
-else:
-    date1_using = datetime.today() - timedelta(days=1)
-    date2_using = datetime.today()
+[1] USOM
+[2] WHOISDS
+[3] DEVAM
 
-#kullanıcıdan oto enter sorgusu
-conf_usom_asking = conf_str.split("\n")[1].split("=")[1]
+- ile seçimi silebilirsiniz.
 
-if conf_usom_asking == "y": usom_asking = True
-else: usom_asking = False
+ """)
+
+print(welcome)
+
+isDevam = False
+isUsom = False
+isWhoisds = False
+
+def show_selected():
+    if(isUsom): print("Usom Seçildi\n")
+    if(isWhoisds): print("Whoisds Seçildi\n")
+
+while not isDevam:
+    s1 = input("Seçim: ")
+    print()
+    if(s1 == "1"):
+        isUsom = True
+        show_selected()
+    elif(s1 == "2"):
+        isWhoisds = True
+        show_selected()
+    elif(s1 == "3"):
+        isDevam = True
+        show_selected()
+    elif(s1 == "-1"):
+        isUsom = False
+        show_selected()
+    elif(s1 == "-2"):
+        isWhoisds = False
+        show_selected()
+    else:
+        print("Yanlış Seçim")
+
+
+if(isUsom):
+    giris = input("Usomun tarih değerleri varsayılan (dün ve bugün) olarak kalsın mı?(y/n)")
+    if giris == "n":
+
+        date1_i = input("Başlangıç Tarihini Giriniz (YIL-AY-GÜN): ")
+        date1_data = date1_i.split("-")
+
+        date2_i = input("Bitiş Tarihini Giriniz (YIL-AY-GÜN): ")
+        dates2_data = date2_i.split("-")
+
+        date1_using = datetime(int(date1_data[0]), int(date1_data[1]), int(date1_data[2]))
+        date2_using = datetime(int(dates2_data[0]), int(dates2_data[1]), int(dates2_data[2]))
+    else:
+        date1_using = datetime.today() - timedelta(days=1)
+        date2_using = datetime.today()
+
+    #kullanıcıdan oto enter sorgusu
+    conf_usom_asking = conf_str.split("\n")[1].split("=")[1]
+
+    if conf_usom_asking == "y": usom_asking = True
+    else: usom_asking = False
 
 #webdriver ayarlama
 browser = webdriver.Chrome(executable_path="./chromedriver.exe")
 browser.maximize_window()
 
-#whoisds işlemleri
-browser.get("https://www.whoisds.com/newly-registered-domains")
-whoisds_formated_date = whoisds_domain_date_format(browser.find_element_by_xpath("/html/body/div[2]/div/div/div/div/div[1]/div[2]/div/table/tbody/tr[1]/td[3]").text)
-path_to_zip_file = conf_default_download_dir+ "newly-registered-domains-" + whoisds_formated_date + ".zip"
+if(isWhoisds):
+    #whoisds işlemleri
+    browser.get("https://www.whoisds.com/newly-registered-domains")
+    whoisds_formated_date = whoisds_domain_date_format(browser.find_element_by_xpath("/html/body/div[2]/div/div/div/div/div[1]/div[2]/div/table/tbody/tr[1]/td[3]").text)
+    path_to_zip_file = conf_default_download_dir+ "newly-registered-domains-" + whoisds_formated_date + ".zip"
 
-browser.find_element_by_xpath("/html/body/div[2]/div/div/div/div/div[1]/div[2]/div/table/tbody/tr[1]/td[4]/a/button").click()
-wait_until_download(path_to_zip_file)
+    browser.find_element_by_xpath("/html/body/div[2]/div/div/div/div/div[1]/div[2]/div/table/tbody/tr[1]/td[4]/a/button").click()
+    wait_until_download(path_to_zip_file)
 
-#usom işlemleri
-browser.get("https://www.usom.gov.tr/")
 
-#xml dosyasını indire basıp 10 saniye bekliyor
-browser.find_element_by_xpath("/html/body/app-root/app-layout/div/div/app-header/header/div/div[1]/div/div/nav[1]/ul/li[2]/a").click()
+if(isUsom):
+    #usom işlemleri
+    browser.get("https://www.usom.gov.tr/")
 
-time.sleep(2)
+    #xml dosyasını indire basıp 10 saniye bekliyor
+    browser.find_element_by_xpath("/html/body/app-root/app-layout/div/div/app-header/header/div/div[1]/div/div/nav[1]/ul/li[2]/a").click()
 
-if usom_asking:
-    print("\nUsomdan dosya indirilirken uyarı veriyor olarak ayarlandı. Değiştirmek için conf dosyasını düzenleyiniz.")
-    pyautogui.keyDown("shift")
-    for i in range(14) :pyautogui.press("tab")
-    pyautogui.keyUp("shift")
-    pyautogui.press("enter")
+    time.sleep(2)
 
-wait_until_download(conf_default_download_dir + "url-list.xml")
+    if usom_asking:
+        print("\nUsomdan dosya indirilirken uyarı veriyor olarak ayarlandı. Değiştirmek için conf dosyasını düzenleyiniz.")
+        pyautogui.keyDown("shift")
+        for i in range(14) :pyautogui.press("tab")
+        pyautogui.keyUp("shift")
+        pyautogui.press("enter")
 
-#whoisds zip dosyasını çıkarma
-with zipfile.ZipFile(path_to_zip_file, 'r') as zip_ref:
-    zip_ref.extractall("./")
+    wait_until_download(conf_default_download_dir + "url-list.xml")
+
+
+if(isWhoisds):
+    #whoisds zip dosyasını çıkarma
+    with zipfile.ZipFile(path_to_zip_file, 'r') as zip_ref:
+        zip_ref.extractall("./")
 
 #keyword listesini içeri alma
 keywords_f = open('./keywords.txt', 'r')
@@ -101,58 +158,74 @@ for keyword in keywords_l:
 keywords_a = keywords_str.split("\n")
 keywords_a.pop()
 
-#whoisds domainlerini okuma
-whoisds_domains_f = open('domain-names.txt', 'r')
-whoisds_domains_l = whoisds_domains_f.readlines()
+if(isWhoisds):
+    #whoisds domainlerini okuma
+    whoisds_domains_f = open('domain-names.txt', 'r')
+    whoisds_domains_l = whoisds_domains_f.readlines()
 
-whoisds_domains_str = ""
+    whoisds_domains_str = ""
 
-for whoisds_domain in whoisds_domains_l:
-    whoisds_domains_str+=whoisds_domain
+    for whoisds_domain in whoisds_domains_l:
+        whoisds_domains_str+=whoisds_domain
 
-#whoisds domainleri dizi haline getiriliyor
-whoisds_domains_a = whoisds_domains_str.split("\n")
+    #whoisds domainleri dizi haline getiriliyor
+    whoisds_domains_a = whoisds_domains_str.split("\n")
 
-#usom domainlerini okuma
-tree = ET.parse(conf_default_download_dir + "url-list.xml")
-root = tree.getroot()
+    if(not isUsom):
+        print("\nAranan Keywordler: ")
+        for b in keywords_a: print(str(b).split(" ")[0])
+    print()
 
-date_rage = pandas.date_range(date1_using,date2_using,freq='d')
+if(isUsom):
+    #usom domainlerini okuma
+    tree = ET.parse(conf_default_download_dir + "url-list.xml")
+    root = tree.getroot()
 
-print("\nBakılan Tarihler: ")
-for a in date_rage: print(str(a).split(" ")[0])
-print("\nAranan Keywordler: ")
-for b in keywords_a: print(str(b).split(" ")[0])
+    date_rage = pandas.date_range(date1_using,date2_using,freq='d')
 
-usom_domains_str = ""
+    print("\nBakılan Usom Tarihleri: ")
+    for a in date_rage: print(str(a).split(" ")[0])
+    print("\nAranan Keywordler: ")
+    for b in keywords_a: print(str(b).split(" ")[0])
+    print()
 
-index_of_date = 0
-for index_of_date in range(len(date_rage)-1):
-    index_of_usom = 0
-    for index_of_usom in range(len(root[1])-1):
-        if (root[1][index_of_usom][4].text).split(" ")[0] == str(date_rage[index_of_date]).split(" ")[0]:
-            usom_domains_str += (root[1][index_of_usom][1].text + "\n")
-        index_of_usom+=1
-    index_of_date+=1
+    usom_domains_str = ""
 
-#usom domainleri dizi haline getiriliyor
-usom_domains_a = usom_domains_str.split("\n")
+    for index_of_date in range(len(date_rage)-1):
+        index_of_usom = 0
+        for index_of_usom in range(len(root[1])-1):
+            if (root[1][index_of_usom][4].text).split(" ")[0] == str(date_rage[index_of_date]).split(" ")[0]:
+                usom_domains_str += "Tarih: " + root[1][index_of_usom][4].text.split(" ")[0] + "  Domain: " + (root[1][index_of_usom][1].text + "\n")
+            index_of_usom+=1
+        index_of_date+=1
+
+    #usom domainleri dizi haline getiriliyor
+    usom_domains_a = usom_domains_str.split("\n")
 
 final_text = ""
 
 for keyword in keywords_a:
     final_text +="\n " +  "*"*10 + keyword + " keywordu " + "*"*10 + "\n\n"
-    final_text +="\n " +  "-"*10 + "whoisds domain" + "-"*10 + "\n"
-    for whoisds_domain in whoisds_domains_a:
-        if keyword in whoisds_domain: final_text += whoisds_domain + "\n"
-    final_text +="\n " +  "-"*10 + "usom domain" + "-"*10 + "\n"
-    for usom_domain in usom_domains_a:
-        if keyword in usom_domain: final_text += usom_domain + "\n"
+    print("\n " +  "*"*10 + keyword + " keywordu " + "*"*10 + "\n\n")
+    if(isWhoisds):
+        final_text +="\n " +  "-"*10 + "whoisds domain" + "-"*10 + "\n"
+        print("\n " +  "-"*10 + "whoisds domain" + "-"*10 + "\n")
+        for whoisds_domain in whoisds_domains_a:
+            if keyword in whoisds_domain: 
+                final_text += whoisds_domain + "\n"
+                print(whoisds_domain)
+    if(isUsom):
+        final_text +="\n " +  "-"*10 + "usom domain" + "-"*10 + "\n"
+        print("\n " +  "-"*10 + "usom domain" + "-"*10 + "\n")
+        for usom_domain in usom_domains_a:
+            if keyword in usom_domain: 
+                final_text += usom_domain + "\n"
+                print(usom_domain)
 
-final_text_f = open("result.txt", "a")
+final_text_f = open("result.txt", "w")
 
 final_text_f.write(final_text)
 
 final_text_f.close()
-whoisds_domains_f.close()
+if(isWhoisds): whoisds_domains_f.close()
 keywords_f.close()
